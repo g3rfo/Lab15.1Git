aad26bc01c3a2d791ceed3cee240b427210e5d66
g3rfo <rozhchenkoivan@gmail.com>
2023-12-12 08:25:23 +0200

    Add project files.

---
+++ Lab15.1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	float num1, num2;
+
+	printf("Enter first number: ");
+	scanf_s("%f", &num1);
+	printf("Enter second number: ");
+	scanf_s("%f", &num2);
+
+	if (num1 > num2)
+		printf("%.2f", num1);
+	else
+		printf("%.2f %.2f", num1, num2);
+
+	return 0;
+}
